5d4ff6dc7330e8ab82fc15533e16a2b81065bcdb
RafaelSotero-dev <rafaeljesusabc2@gmail.com>
2023-07-14 19:51:51 -0300

    chore: changed controller folder name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { config } from 'dotenv';
+import { GetAllUsersMongoRepository } from './repositories/get-all-users-repository/get-all-users-mongo-repository';
+import { GetAllUsersController } from './controllers/get-all-users/get-all-users';
+import { mongoDb } from './database/mongo';
 
 config();
 
@@ -9,11 +12,38 @@ class App {
 
 	constructor() {
 		this.router = express();
+		this.route();
 		this.start();
 	}
 
 	public static main(): void {
-		new App();
+		try {
+			mongoDb.connect();
+			new App();
+		} catch (error) {
+			console.log(error);
+		}
+
+	}
+
+	private route(): void {
+		this.router.use(express.json());
+
+		this.router.get('/users', async (_req: Request, res: Response) => {
+			try {
+				const getAllUserMongoRepository = new GetAllUsersMongoRepository();
+				const getAllUsersController = new GetAllUsersController(getAllUserMongoRepository);
+
+				const {body, status} = await getAllUsersController.handle();
+
+				return res.status(status).json(body);
+
+			} catch (error) {
+				const e = error as Error;
+				return res.status(500).json(e.message);
+			}
+		});
+
 	}
 
 	private start(): void {
