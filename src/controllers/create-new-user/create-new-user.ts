8bdc6ef70a45b011d9c1c223bfa50079b3d6e605
RafaelSotero-dev <rafaeljesusabc2@gmail.com>
2023-07-15 22:40:42 -0300

    feat: create new user field validation

--- src/controllers/create-new-user/create-new-user.ts
+++ src/controllers/create-new-user/create-new-user.ts
@@ -1,6 +1,8 @@
 import { IUserInput } from '../../models/User';
 import { ErrorHandler } from '../../utils/ErrorHandler';
 import { createToken } from '../../utils/createToken';
+import { validateEmail } from '../../utils/emailValidator';
+import { passwordEncrypt } from '../../utils/passwordEncrypt';
 import { httpRequest, httpResponse } from '../protocols';
 import { ICreateNewUserController, ICreateNewUserRepository } from './protocols';
 
@@ -8,7 +10,7 @@ export class CreateNewUserController implements ICreateNewUserController {
 	
 	constructor(private createNewUserRepository: ICreateNewUserRepository) {}
 
-	async handle(params: httpRequest<IUserInput>): Promise<httpResponse<string>> {
+	async handle(params: httpRequest<IUserInput, unknown, unknown>): Promise<httpResponse<string>> {
 		const fildsRequired: Array<keyof IUserInput> = ['name', 'email', 'password'];
     
 		if (!params.body) {
@@ -21,7 +23,25 @@ export class CreateNewUserController implements ICreateNewUserController {
 			}
 		}
 
-		const result = await this.createNewUserRepository.create(params.body);
+		if (!validateEmail(params.body.email)){
+			throw new ErrorHandler('Invalid email format', 401);
+		}
+
+		if (params.body.password.length < 6){
+			throw new ErrorHandler('Password must be at least 6 characters long', 401);
+		}
+
+		if (params.body.name.length < 1) {
+			throw new ErrorHandler('Name must be at least 4 characters long', 401);
+		}
+
+		const hash = await passwordEncrypt(params.body.password);
+
+		const result = await this.createNewUserRepository.create({
+			name: params.body.name,
+			email: params.body.email,
+			password: hash
+		});
 
 		if (result === 'user already exist') {
 			throw new ErrorHandler('user already exist', 400);
